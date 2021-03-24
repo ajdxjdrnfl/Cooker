a91a380ec951dff240ca689d05c9e3305ff8e6a0
ajdxjdrnfl <zunghoon4023@gmail.com>
2021-03-25 01:24:27 +0900

    add bottomNavigation(1/2)

---
+++ app/src/main/java/korea/co/cooker/FragmentIngredient.java
@@ -0,0 +1,21 @@
+package korea.co.cooker;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.zip.Inflater;
+
+public class FragmentIngredient extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_ingredient, container, false);
+        return rootView;
+    }
+}
